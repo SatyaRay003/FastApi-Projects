26d0c08b001bc38b0604e0e3e6bf47882b8ba2d5
SatyaRay003 <satya.roy003@gmail.com>
2022-07-19 01:23:46 +0530

    Update Code

---
+++ library_book_management/test/response.py
@@ -0,0 +1,16 @@
+get_all_book = {
+    "code": "200",
+    "status": "Ok",
+    "message": "Success fetch all data",
+    "result": [
+        {
+            "genre_name": "Detective Story",
+            "book_name": "Bomkesh Bakshi Part 3",
+            "author": "Sharadindu Bandyopadhyay",
+            "copy_count": 45,
+            "author_id": 2,
+            "book_id": 6,
+            "genre_id": 123
+        }
+    ]
+}
